ae5ac4d3b1c6370df637e9f0ddf400b9bc7540ae
Rohit Shaw <rohitshaw509@gmail.com>
2022-04-21 09:09:23 +0530

    Database setup

---
+++ Backend/controllers/posts.js
@@ -0,0 +1,16 @@
+import PostMessage from "../models/posyMessage.js";
+
+export const getPosts= async (req, res)=>{
+    try{
+        const postMessage=await PostMessage.find();
+
+        res.status(200).json(postMessage);
+    }
+    catch(err){
+        res.status(404).json({message : err.message})
+    }
+}
+
+export const createPost=(req,res)=>{
+    res.send('Post Creation');
+}
--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,26 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoues from './routes/posts.js'
+
 
 const app = express();
 
-app.use(bodyParser.jason({limit: "30mb",extended:true}));
+
+app.use(bodyParser.json({limit: "30mb",extended:true}));
 app.use(bodyParser.urlencoded({limit: "30mb",extended:true}));
 
 app.use(cors());
+
+app.use('/posts',postRoues);
+
+const CONNECTION_URL = 'mongodb+srv://socialmedia:Rohit123@cluster0.rr4rf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+ const PORT =process.env.PORT || 5000;
+
+ mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+ 
+//   mongoose.set('useFindAndModify', false);
+
